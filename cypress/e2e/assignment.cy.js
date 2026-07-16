bd900df2cc9bbb920d515d04a38d5e4057111a02
Gayathri <gayathri.r.nair89@gmail.com>
2026-07-16 09:42:26 +0400

    Added Testcases 9,10,11

--- cypress/e2e/assignment.cy.js
+++ cypress/e2e/assignment.cy.js
@@ -591,3 +591,101 @@ describe('Test Case 8: Shadow DOM Element Interactions', () => {
       .should('have.value', inputValue);
   });
 });
+
+// ===========================================
+// Test Case 9: Hidden elements
+// ===========================================
+
+describe('Test case 9 : Hidden Elements', () => {
+ const homePage =
+    'https://testautomationpractice.blogspot.com/';
+
+  it('Show and interact with hidden input field', () => {
+    const sampleText = 'Cypress hidden element test';
+
+    // Step 1: Open the main page
+    cy.visit(homePage);
+
+    // Step 2: Navigate to the Footer Links section
+    cy.contains('h2', 'Footer Links')
+      .scrollIntoView()
+      .should('be.visible');
+
+    // Step 3: Open the Hidden Elements & AJAX page
+    cy.contains('a', 'Hidden Elements & AJAX')
+      .should('be.visible')
+      .click();
+
+    //Step 4: Verify the URL is opened
+    cy.url()
+    .should('include','/p/gui-elements-ajax-hidden.html')
+
+    //Step 5: Find the label for input box 2
+    cy.get('#input2')
+    .should('exist')
+    .and('not.be.visible');
+
+    //Step 6: Click the control that reveals the hidden input field. 
+    cy.get('[id="toggleInput"]')
+    .click();
+
+    //Step 7 :Verify the input field becomes visible. 
+    cy.get('[id="input2"]')
+    .eq(0)
+    .should('be.visible')
+    .type(sampleText)
+    .should("have.value",sampleText);
+
+  })
+})
+
+describe('Test case 10 :  Show and select hidden checkbox', () => {
+ const homePage =
+    'https://testautomationpractice.blogspot.com/';
+
+  it('Show and interact with hidden input field', () => {
+    const sampleText = 'Cypress hidden element test';
+
+    // Step 1: Open the main page
+    cy.visit(homePage);
+    
+    //Step 2 : Verify the checkbox becomes visible. 
+    cy.get('[id="toggleCheckbox"]')
+    .should('be.visible')
+    .click()
+
+    //Step 3: Select the checkbox. 
+     cy.get('[id="checkbox2"]')
+     .should('be.visible')
+     .check();
+    
+  })
+})
+
+describe('Drag and Drop Feature', () => {
+  it('Drag an item to a drop target', () => {
+    cy.visit('https://testautomationpractice.blogspot.com/');
+
+    //Step 1: Navigate to the drag-and-drop section. 
+    cy.get('#draggable')
+      .should('be.visible');
+
+    cy.get('#droppable')
+      .should('be.visible')
+      .and('contain.text', 'Drop here');
+
+    //Step 2: Drag the source element. 
+    cy.get('#draggable')
+      .trigger('mousedown',{which: 1});
+
+    cy.get('#droppable')
+      .trigger('mousemove')
+      .trigger('mouseup', { force: true });
+
+    cy.get('#droppable')
+      .should('contain.text', 'Dropped!');
+  });
+});
+
+
+
